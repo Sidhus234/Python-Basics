3ad6a78e16e38c0d8aa915b4b1b44f311d040bd2
Gursewak Singh Sidhu <sidhus234@gmail.com>
2022-04-27 08:17:46 +0800

    Create resize_all_images.py

---
+++ Image and Video Processing/resize_all_images.py
@@ -0,0 +1,13 @@
+"""Resizes all the images in a directory to 100 by 100"""
+import cv2
+import glob
+
+images = glob.glob(".//Images//*.jpg")
+
+for image in images:
+    img = cv2.imread(image, 0)
+    re = cv2.resize(img, (100, 100))
+    cv2.imshow("Hey", re)
+    cv2.waitKey(500)
+    cv2.destroyAllWindows()
+    cv2.imwrite("resized_"+image, re)
